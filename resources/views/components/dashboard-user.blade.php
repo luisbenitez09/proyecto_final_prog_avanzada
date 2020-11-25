7bfb078e2abf6be7f4e173cf55aedad4f97c132f
mariameza99 <54654158+mariameza99@users.noreply.github.com>
2020-11-25 00:32:08 -0700

    Se empezo diseño dashboard user

--- resources/views/components/dashboard-user.blade.php
+++ resources/views/components/dashboard-user.blade.php
@@ -1,4 +1,5 @@
 <div>
+	
     <div class="wrapper d-flex align-items-stretch">
 		<nav id="sidebar" class="active">
 			<div class="custom-menu">
@@ -8,19 +9,19 @@
 				</button>
 			</div>
 			<div class="p-4">
-				<h1><a href="index.html" class="logo">Flash</a></h1>
+				<img src="img/logo.svg" alt="BlockBuster2" id="nav-logo">
 				<ul class="list-unstyled components mb-5">
 					<li class="active">
 						<a href="#"><span class="fa fa-home mr-3"></span> Home</a>
 					</li>
 					<li>
-						<a href="#"><span class="fa fa-user mr-3"></span> About</a>
+						<a href="#"><span class="fa fa-user mr-3"></span> Profile</a>
 					</li>
 					<li>
-						<a href="#"><span class="fa fa-briefcase mr-3"></span> Portfolio</a>
+						<a href="#"><span class="fa fa-briefcase mr-3"></span> Rent Movies</a>
 					</li>
 					<li>
-						<a href="#"><span class="fa fa-sticky-note mr-3"></span> Blog</a>
+						<a href="#"><span class="fa fa-sticky-note mr-3"></span> Rented Movies</a>
 					</li>
 					<li>
 						<a href="#"><span class="fa fa-paper-plane mr-3"></span> Contact</a>
@@ -28,7 +29,7 @@
 				</ul>
 
 				<div class="mb-5">
-					<h3 class="h6 mb-3">Subscribe for newsletter</h3>
+					<h3 class="h6 mb-3">Subscribe for BlockBuster Infinite</h3>
 					<form action="#" class="subscribe-form">
 						<div class="form-group d-flex">
 							<div class="icon"><span class="icon-paper-plane"></span></div>
@@ -50,7 +51,40 @@
 
 			</div>
 		</nav>
-
+		
+		<div class="dashboard_user">
+			<div class="container">
+			<div class="row">
+				<div class="col-sm">
+				<div class="card bg-dark text-white">
+					<img src="img/bob_ok.jpg" class="card-img" alt="...">
+					<div class="card-img-overlay">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text"></p>
+					</div>
+					</div>
+				</div>
+				<div class="col-sm">
+				<div class="card bg-dark text-white">
+					<img src="img/movie.jpg" class="card-img" alt="...">
+					<div class="card-img-overlay">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Parasite.</p>
+					</div>
+					</div>
+				</div>
+				<div class="col-sm">
+				<div class="card bg-dark text-white">
+					<img src="img/bob_ok.jpg" class="card-img" alt="...">
+					<div class="card-img-overlay">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text"></p>
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
 
 	</div>
 </div>
