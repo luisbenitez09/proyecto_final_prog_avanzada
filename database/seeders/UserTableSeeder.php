a35899eb8c3d1c63d4ff1c650ff0b1be9662843a
mariameza99 <54654158+mariameza99@users.noreply.github.com>
2020-12-04 09:59:32 -0700

    Se agregaron más users y loans para pruebas

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -29,5 +29,85 @@ class UserTableSeeder extends Seeder
         $user->password = bcrypt("12345678");
         $user->role_id = "1";
         $user->save();
+
+        $user = new User();
+        $user->name = "Andrea";
+        $user->lastname = "Peréz";
+        $user->email = "andrea@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Angel";
+        $user->lastname = "Ortega";
+        $user->email = "angel@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Silvia";
+        $user->lastname = "Rodriguez";
+        $user->email = "silvia@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Miguel";
+        $user->lastname = "Benitez";
+        $user->email = "miguel@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Heidi";
+        $user->lastname = "Rábago";
+        $user->email = "heidi@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Susana";
+        $user->lastname = "Hernandez";
+        $user->email = "susana@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Reynaldo";
+        $user->lastname = "Manríquez";
+        $user->email = "reynaldo@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Corina";
+        $user->lastname = "Díaz";
+        $user->email = "corina@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Armando";
+        $user->lastname = "Tercero";
+        $user->email = "armando@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Javier";
+        $user->lastname = "Cota";
+        $user->email = "javier@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->role_id = "2";
+        $user->save();
     }
 }
